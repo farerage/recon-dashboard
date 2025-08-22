2e7611d78de373ca3bd6c39773280eee1ae29c74
farerage <fareragerry21@gmail.com>
2025-08-22 14:15:17 +0700

    update app to use Supabase

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
 
 DB_USER = "postgres.ivayfglmtlnnbaxotc"
-DB_PASS = "bambang17"   # ganti dengan password DB kamu
+DB_PASS = "WmOBSqEwqdVLBcvc"   # ganti dengan password DB kamu
 DB_HOST = "aws-1-ap-southeast-1.pooler.supabase.com"
 DB_PORT = "6543"
 DB_NAME = "postgres"
