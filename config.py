18a4833a7660d91d920678bf6bccaab38cf107d2
farerage <fareragerry21@gmail.com>
2025-08-22 14:16:35 +0700

    update app to use Supabase

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ DB_USER = "postgres.ivayfglmtlnnbaxotc"
 DB_PASS = "WmOBSqEwqdVLBcvc"   # ganti dengan password DB kamu
 DB_HOST = "aws-1-ap-southeast-1.pooler.supabase.com"
 DB_PORT = "6543"
-DB_NAME = "postgres"
+DB_NAME = "reconku"
